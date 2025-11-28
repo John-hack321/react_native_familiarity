3fc8ccf7537e5a98ce8c644c6d92ba35c12dc8b0
johtieno6@gmail.com <johtieno6@gmail.com>
2025-11-28 20:32:10 +0300

    solving issues on code

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { FlatList, Pressable, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { offers } from "./constants/constants";
+import cn from 'clsx';
+import { Fragment } from "react";
+import { Image } from "react-native";
+import { offers } from "../constants/constants";
 
 export default function Index() {
   return (
@@ -9,9 +12,31 @@ export default function Index() {
       <FlatList
       data= {offers}
       renderItem={({item, index})=> {
+        
+        const isEven: boolean= index % 2 ===0;
+
         return (
           <View>
-            <Pressable className="bg-amber-600">
+            {/* this is the offer card */}
+            <Pressable
+            style= {{backgroundColor: item.color}}
+             className= {cn(
+              "w-full h-48 my-3 rounded-xl overflow-hidden shadow-lg flex items-center gap-5", 
+              isEven ? 'flex-row-reverse' : 'flex-row')}>
+              {({pressed})=> (
+                <Fragment>
+                  <View className= {'h-1/2 w-full'}>
+                    <Image source={item.image} className={'size-full'} resizeMode={'contain'}></Image>
+                  </View>
+
+                  {/* offer card info will go here */}
+                  <View className="flex-1 h-full flex flex-col justify-center items-start gap-4">
+                    <Text className="text-lg text-black">
+                      {item.title}
+                    </Text>
+                  </View>
+                </Fragment>
+              )}
             </Pressable>
           </View>
         )
---
+++ constants/constants.ts
@@ -0,0 +1,27 @@
+
+export const offers = [
+    {
+        id: 1,
+        title: "SUMMER COMBO",
+        image: require("../assets/images/burger image.png"),
+        color: "#D33B0D"
+    },
+    {
+        id: 2,
+        title: "BURGER BASH",
+        image: require("../assets/images/tortilas.png"),
+        color: "#DF5A0C"
+    },
+    {
+        id: 4,
+        title: "BURRITO DELIGHT",
+        image: require("../assets/images/pizza.png"),
+        color: "#EB920C"
+    },
+    {
+        id: 5,
+        title: "FRENCH FRIES",
+        image: require("../assets/images/frenchFries.png"),
+        color: '#2E8B57'
+    }
+]
